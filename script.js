a3403a9d19e99263fb41fc25babb22ab60c120e5
henriB <henri.bretseau@gmail.com>
2020-11-23 17:57:28 +0100

    demain dynamique + value + min date départ

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ console.log(today); //Année-Mois-Jour
 start.value=today;
 start.min = today;
 
+const tomorrowDate = ()=>{
+    let day = new Date(today);
+    day.setDate(day.getDate()+1);
+    let tomorrow = day.toISOString().split('T')[0];
+    end.value= tomorrow;
+    end.min = tomorrow;
+}
+
+tomorrowDate();
