11d116e2793e4da9b0d01d57d2ab8984551fe4da
henriB <henri.bretseau@gmail.com>
2020-11-23 17:53:33 +0100

    date

---
+++ script.js
@@ -0,0 +1,4 @@
+const start = document.getElementById('start_date');
+const end = document.getElementById('end_date');
+const today= new Date().toISOString().split('T')[0];
+console.log(today); //Année-Mois-Jour
