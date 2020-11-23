13458ca8bb2e5679217ffae347e645a9719a1c77
henriB <henri.bretseau@gmail.com>
2020-11-23 17:56:45 +0100

    value + min pour date arrivé

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const start = document.getElementById('start_date');
 const end = document.getElementById('end_date');
 const today= new Date().toISOString().split('T')[0];
 console.log(today); //Année-Mois-Jour
+
+start.value=today;
+start.min = today;
+
